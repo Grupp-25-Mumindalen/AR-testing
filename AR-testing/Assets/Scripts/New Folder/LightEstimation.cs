8b0b77e60fd905ad8cd21a8afff1e93a52fd8724
Julia <julia.v.wang@gmail.com>
2022-03-09 00:01:12 +0100

    Not much has been done
    but switching the Camera mode and beginning a script

---
+++ AR-testing/Assets/Scripts/New Folder/LightEstimation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightEstimation : MonoBehaviour
+{
+
+    private Light lit; // This is kinda LIT ngLie(t)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lit = GetComponent<Light>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    /// METHODS AND SUCH
+}
